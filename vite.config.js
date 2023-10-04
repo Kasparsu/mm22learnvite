dbc940beb389ccb932884e3f7577be0d824b53e6
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-04 16:40:51 +0300

    Add twig support

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import vituum from 'vituum'
+import twig from '@vituum/vite-plugin-twig'
+export default {
+    plugins: [
+        vituum(),
+        twig({
+            root: './src'
+        })
+    ]
+}
