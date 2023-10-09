2fb687d06b4aef0810d0090fe15c082ed88dbec2
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-09 09:09:47 +0300

    Add svelte

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    let count = 0;
+    function click(){
+        count++;
+    }
+</script>
+
+<button class="btn btn-primary" on:click={click}>Click me!</button>
+<h1>{ count }</h1>
+
+<style>
+
+</style>
