1c736c32c41e2752053269f3fc43be35e4fbb720
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-09 10:28:31 +0300

    Add itemlist example

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,31 @@
 <script>
-    let count = 0;
-    function click(){
-        count++;
+    import ItemList from "./ItemList.svelte";
+    let message = '';
+    let items = [
+        { name: 'leib', isDone: true },
+        { name: 'sai', isDone: false },
+        { name: 'piim', isDone: false },
+        { name: 'viin', isDone: true },
+    ];
+    $: doneItems = items.filter(item => item.isDone);
+    $: todoItems = items.filter(item => !item.isDone);
+    function addItem(){
+        items.push(message);
+        items=items;
+        message='';
     }
+
 </script>
 
-<button class="btn btn-primary" on:click={click}>Click me!</button>
-<h1>{ count }</h1>
-
+<div class="container">
+    <div class="input-group mt-3">
+        <input type="text" class="form-control" bind:value={message}>
+        <button class="btn btn-primary" on:click={addItem}>Click me!</button>
+    </div>
+    <ItemList title="All Items" bind:items={items} />
+    <ItemList title="Done Items" bind:items={doneItems}/>
+    <ItemList title="Todo Items" bind:items={todoItems}/>
+</div>
 <style>
 
 </style>
---
+++ src/ItemList.svelte
@@ -0,0 +1,13 @@
+<script>
+
+    export let title;
+    export let items;
+
+</script>
+
+<h1>{title}</h1>
+<ul>
+    {#each items as item}
+        <li>{item.name} <input type="checkbox" class="form-check-input" bind:checked={item.isDone} ></li>
+    {/each}
+</ul>
